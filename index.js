319a5c8eddbd6ab57076a5afa4ddf0c6577a9f76
Zulk <65410126+Zulkifli70@users.noreply.github.com>
2025-06-07 10:47:19 +0700

    Change size input from using prompt to input field
    Create input field, button, and input condition when its null, number is below 1 or above user should input again a number between 1 and 100.

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
+const input = document.querySelector(".input");
+const button = document.querySelector(".button");
+
 const container = document.querySelector(".container");
 container.setAttribute("class", "container");
 
-const sketch = document.createElement("div");
-sketch.setAttribute("class", "sketch");
+function createGrid(size) {
+  container.innerHTML = "";
+  const sketchSize = 960 / size;
+  for (let i = 1; i <= size * size; i++) {
+    const sketch = document.createElement("div");
+    sketch.setAttribute("class", "sketch");
 
-const input = prompt("Input number between 1-100");
-const total = input * input;
-const sketchSize = 960 / input;
-for (let i = 1; i <= total; i++) {
-  const sketch = document.createElement("div");
-  sketch.setAttribute("class", "sketch");
+    sketch.style.width = `${sketchSize}px`;
+    sketch.style.height = `${sketchSize}px`;
 
-  sketch.style.width = `${sketchSize}px`;
-  sketch.style.height = `${sketchSize}px`;
-
-  sketch.addEventListener("mouseenter", () => {
-    sketch.classList.add("hovered");
-  });
-  container.appendChild(sketch);
+    sketch.addEventListener("mouseenter", () => {
+      sketch.classList.add("hovered");
+    });
+    container.appendChild(sketch);
+  }
 }
+
+function createNewGrid() {
+  const UserInput = input.value;
+
+  if (UserInput === null) {
+    return alert("Please fill with a number");
+  }
+
+  const size = parseInt(UserInput);
+
+  if (isNaN(size)) {
+    alert("Please input a correct number");
+    return;
+  }
+
+  if (size < 1 || size > 100) {
+    alert("number must between 1-100");
+    return;
+  }
+
+  createGrid(size);
+}
+
+button.addEventListener("click", createNewGrid);
+
+createGrid(16);
